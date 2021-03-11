405109ef1afe9433d1d71b4a01ef92a0d6ce45eb
Levin Keller <post@levinkeller.de>
2021-03-11 09:39:28 +0100

    fix email port

--- api/settings.py
+++ api/settings.py
@@ -56,7 +56,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 2525))
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 WHITELIST_EMAILS = os.environ.get('WHITELIST_EMAILS', '').split(',')
